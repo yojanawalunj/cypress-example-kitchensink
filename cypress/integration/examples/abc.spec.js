b51d558b68ee13f5b04abb4f4cda0ad42cea355c
yojanawalunj <42763255+yojanawalunj@users.noreply.github.com>
2019-01-08 12:03:50 +0530

    Update abc.spec.js

--- cypress/integration/examples/abc.spec.js
+++ cypress/integration/examples/abc.spec.js
@@ -6,9 +6,9 @@ context('Aliasing', () => {
             .type('jharris')
         cy.get('.m-form__group').find('input[type=password]').first()
             .type('Bip$2018')
-        cy.wait(8000)
+        
         cy.get('#m_login_signin_submit').click()
-        cy.wait(8000)
+        
         cy.get('#m_aside_left_offcanvas_toggle').click()
        
     })
