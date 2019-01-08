18e27edeb0ab398d251bddc7de07e47690b4c55c
yojanawalunj <42763255+yojanawalunj@users.noreply.github.com>
2019-01-08 11:50:24 +0530

    Update abc.spec.js

--- cypress/integration/examples/abc.spec.js
+++ cypress/integration/examples/abc.spec.js
@@ -9,11 +9,20 @@ context('Aliasing', () => {
         cy.wait(8000)
         cy.get('#m_login_signin_submit').click()
         cy.wait(8000)
+        cy.get('#m_aside_left_offcanvas_toggle').click()
        
     })
     it('fees review and profile', () => {
         //open the fee profile page 
         cy.wait(3000)
+        cy.contains('Clients').click()
+        cy.get('input[type=search]')//search for the household and open that client page 
+            .type('Parikh, Nishith')
+            .then((val) => {
+                if (val) {
+                    cy.get('.sorting_1').contains('Parikh, Nishith').click()
+                }
+            })
 
     })
 })
